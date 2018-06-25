cee8b96193a27de2e5be934b376dbebb7183e986
saurav <skuwar@olivemedia.co>
2018-06-25 12:23:12 +0545

    node modules removed

--- 238 redux-02-basic-flow/redux--02-basic-flow/src/index.js
+++ 238 redux-02-basic-flow/redux--02-basic-flow/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/results';
+import { rootReducer } from './store/reducers';
 
-const rootReducer = combineReducers({
-    ctr: counterReducer,
-    res: resultReducer
-});
 const store = createStore(rootReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+
 registerServiceWorker();
